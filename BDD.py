c2d1bd892efd475616f440514ab1213ea2f77bc4
Drayer Sivertsen <drayer.sivertsen@wsu.edu>
2022-04-13 00:31:27 -0700

    version 3

--- BDD.py
+++ BDD.py
@@ -22,19 +22,46 @@ class graph:
 
     def boolean_R(self):
         expression = ""
-        for i in self.gdict:
-            binary_key = format(self.gdict.keys(), '05b')
+        dict_list = list(self.gdict.items())
+
+        new_dict_list = []
+        for dict_tuple in dict_list:
+            new_dict_list.append((dict_tuple[0], dict_tuple[1][0]))
+            new_dict_list.append((dict_tuple[0], dict_tuple[1][1]))
+
+        for i in range(len(new_dict_list)):
+            duo = new_dict_list[i]
+            binary_key = format(duo[0], '05b')
             expression += "("
             for j in range(len(binary_key)):
                 if binary_key[j] == "0":
                     expression += "~xx" + str(j)
                 else:
-                    formula += "xx" + str(j)
+                    expression += "xx" + str(j)
                 if j != len(binary_key) - 1:
                     expression += " & "
             expression += ")"
+
+            expression += " & "
+
+            binary_key = format(duo[1], '05b')
+            expression += "("
+            for j in range(len(binary_key)):
+                if binary_key[j] == "0":
+                    expression += "~yy" + str(j)
+                else:
+                    expression += "yy" + str(j)
+                if j != len(binary_key) - 1:
+                    expression += " & "
+            expression += ")"
+
             if self.gdict.keys() != 31:
                 expression += " | "
+        expression = expression[:-3]
+
+        print(expression)
+
+        return expression
 
 
 R = graph()
@@ -47,36 +74,18 @@ for i in range(0, 32):
 
 
 xx1, xx2, xx3, xx4, xx5, yy1, yy2, yy3, yy4, yy5 = map(
-    exprvar, "xx1, xx2, xx3, xx4, xx5, yy1, yy2, yy3, yy4, yy5".split())
+    bddvar, "xx1, xx2, xx3, xx4, xx5, yy1, yy2, yy3, yy4, yy5".split())
 
 even_nums = [val for val in range(32) if val % 2 == 0]  # array of even numbers
 
 prime_nums = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]  # array of prime numbers
 
+# R.printGraph()
 
 def boolean_even(even_nums):
     formula = ""
     for i in range(len(even_nums)):
         binary = format(even_nums[i], '05b')
-        formula += "("
-        for j in range(len(binary)):
-            if binary[j] == "0":
-                formula += "~xx" + str(j)
-            else:
-                formula += "xx" + str(j)
-            if j != len(binary) - 1:
-                formula += " & "
-        formula += ")"
-        if i != len(even_nums) - 1:
-            formula += " | "
-
-    print('\n\n' + "EVEN: " + formula + '\n\n')
-    return formula
-
-def boolean_prime(prime_nums):
-    formula = ""
-    for i in range(len(prime_nums)):
-        binary = format(prime_nums[i], '05b')
         formula += "("
         for j in range(len(binary)):
             if binary[j] == "0":
@@ -86,19 +95,98 @@ def boolean_prime(prime_nums):
             if j != len(binary) - 1:
                 formula += " & "
         formula += ")"
+        if i != len(even_nums) - 1:
+            formula += " | "
+
+    # print('\n\n' + "EVEN: " + formula + '\n\n')
+    return formula
+
+def boolean_prime(prime_nums):
+    formula = ""
+    for i in range(len(prime_nums)):
+        binary = format(prime_nums[i], '05b')
+        formula += "("
+        for j in range(len(binary)):
+            if binary[j] == "0":
+                formula += "~xx" + str(j)
+            else:
+                formula += "xx" + str(j)
+            if j != len(binary) - 1:
+                formula += " & "
+        formula += ")"
         if i != len(prime_nums) - 1:
             formula += " | "
 
-    print("PRIME: " + formula + '\n\n')
+    # print("PRIME: " + formula + '\n\n')
     return formula
 
 
 
 
-EVEN = expr(boolean_even(even_nums))
-PRIME = expr(boolean_prime(prime_nums))
+EVEN_ = expr(boolean_even(even_nums))
+PRIME_ = expr(boolean_prime(prime_nums))
+RR_ = expr(R.boolean_R())
 
-EVEN = expr2bdd(EVEN)
-PRIME = expr2bdd(PRIME)
+EVEN = expr2bdd(EVEN_)
+PRIME = expr2bdd(PRIME_)
+RR = expr2bdd(RR_)
 
 
+# step3.1. Obtain BDDs RR, EV EN , P RIM E for the finite sets R, [even], [prime] , respectively.
+# Pay attention to the use of BDD variables in your BDDs. Your code shall also verify the following
+# test cases:
+
+# RR(27, 3) is true;
+num1 = format(27, '05b')
+num2 = format(3, '05b')
+
+if(RR.restrict({xx1:int(num1[0]), xx2:int(num1[1]), xx3:int(num1[2]), xx4:int(num1[3]), xx5:int(num1[4]), yy1:int(num2[0]), 
+yy2:int(num2[1]), yy3:int(num2[2]), yy4:int(num2[3]), yy5:int(num2[4])})):
+    print("RR(27, 3) is true\n")
+else:
+    print("RR(27, 3) is false\n")
+
+# RR(16, 20) is false;
+num1 = format(16, '05b')
+num2 = format(20, '05b')
+
+if(RR.restrict({xx1:int(num1[0]), xx2:int(num1[1]), xx3:int(num1[2]), xx4:int(num1[3]), xx5:int(num1[4]), yy1:int(num2[0]), 
+yy2:int(num2[1]), yy3:int(num2[2]), yy4:int(num2[3]), yy5:int(num2[4])})):
+    print("RR(16, 20) is true\n")
+else:
+    print("RR(16, 20) is false\n")
+# EVEN (14) is true;
+num2 = format(14, '05b')
+
+if(EVEN.restrict({yy1:int(num2[0]), yy2:int(num2[1]), yy3:int(num2[2]), yy4:int(num2[3]), yy5:int(num2[4])})):
+    print("EVEN(14) is true\n")
+else:
+    print("EVEN(14) is false\n")
+# EVEN (13) is false;
+num2 = format(13, '05b')
+
+if(EVEN.restrict({yy1:int(num2[0]), yy2:int(num2[1]), yy3:int(num2[2]), yy4:int(num2[3]), yy5:int(num2[4])})):
+    print("EVEN(13) is true\n")
+else:
+    print("EVEN(13) is false\n")
+# PRIME(7) is true;
+num1 = format(7, '05b')
+
+if(PRIME.restrict({xx1:int(num1[0]), xx2:int(num1[1]), xx3:int(num1[2]), xx4:int(num1[3]), xx5:int(num1[4])})):
+    print("PRIME(7) is true\n")
+else:
+    print("PRIME(7) is false\n")
+# PRIME(2) is false.
+num1 = format(2, '05b')
+
+if(PRIME.restrict({xx1:int(num1[0]), xx2:int(num1[1]), xx3:int(num1[2]), xx4:int(num1[3]), xx5:int(num1[4])})):
+    print("PRIME(2) is true\n")
+else:
+    print("PRIME(2) is false\n")
+
+
+# step3.2. Compute BDD RR2 for the set R ◦R, from BDD RR. Herein, RR2 encodes the set of
+# node pairs such that one can reach the other in two steps. Your code shall also verify the following
+# test cases:
+# RR2(27, 6) is true;
+# RR2(27, 9) is false.
